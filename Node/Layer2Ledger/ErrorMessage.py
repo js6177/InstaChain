c14778c3a39d1a05517a06e24439f6c03b2e726e
James S <jamie.77.smith@protonmail.com>
2021-11-12 23:48:55 -0800

    add alphanumeric checks, make add fees transactional, add flag for enabling/disabling transaction duration logging

--- Node/Layer2Ledger/ErrorMessage.py
+++ Node/Layer2Ledger/ErrorMessage.py
@@ -14,6 +14,7 @@ ERROR_FEATURE_NOT_SUPPORTED = 20
 ERROR_NEGATIVE_AMOUNT = 21
 ERROR_AMOUNT_LESS_THAN_FEE = 22
 ERROR_ADDRESS_LOCKED = 23
+ERROR_NOT_ALPHANUMERIC = 24
 
 error_mapping = {
 ERROR_SUCCESS: "Success",
@@ -31,7 +32,8 @@ ERROR_DATABASE_TRANSACTIONAL_ERROR: "Transaction failed due to server side DB lo
 ERROR_FEATURE_NOT_SUPPORTED: "This feature is not supported",
 ERROR_NEGATIVE_AMOUNT: "The transaction amount or fee cannot be negative",
 ERROR_AMOUNT_LESS_THAN_FEE: "The amount must be greater than the fee",
-ERROR_ADDRESS_LOCKED: "The address is locked because it is being processed by another transaction. Please try again"
+ERROR_ADDRESS_LOCKED: "The address is locked because it is being processed by another transaction. Please try again",
+ERROR_NOT_ALPHANUMERIC: "Transaction ids and addresses need to be alphanumeric"
 }
 
 def build_error_message(error_code, error_message = ''):
