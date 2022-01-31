931ee8cfa9c27a180a1170533e1c8300044966c3
James S <jamie.77.smith@protonmail.com>
2022-01-30 22:18:37 -0800

    add health_check_interval to redis connection

--- Node/Layer2Ledger/RedisInterface.py
+++ Node/Layer2Ledger/RedisInterface.py
@@ -4,7 +4,7 @@ import os
 redis_host = os.environ.get('REDISHOST', 'localhost')
 redis_port = int(os.environ.get('REDISPORT', 6379))
 redis_auth = os.environ.get('REDISAUTH', '')
-redis_client = redis.Redis(host=redis_host, port=redis_port, password=redis_auth, decode_responses=True)
+redis_client = redis.Redis(host=redis_host, port=redis_port, password=redis_auth, decode_responses=True, health_check_interval=30)
 
 def set(key, val):
 	return redis_client.set(key, int(val))
