935a6cad7767ce02ba4f286319f89f6b54423b2d
James S <jamie.77.smith@protonmail.com>
2022-03-19 01:15:51 -0700

    add deposit_address_derivation_path to getNodeInfo api

--- Node/Layer2Ledger/NodeInfoAPI.py
+++ Node/Layer2Ledger/NodeInfoAPI.py
@@ -2,10 +2,11 @@ import os
 import ErrorMessage
 from InstaChainAPI import InstachainRequestHandler
 
-
 #unique randomly generated alphanumeric string valid for the lifetime of the node + ledger
 #used as a nonce for signing transactions to prevent cross-node relay attacks, has no cryptographic value 
 NODE_ID = os.environ.get('NODE_ID')
+
+DEPOSIT_WALLET_MASTER_PUBKEY = os.environ.get('DEPOSIT_WALLET_MASTER_PUBKEY')
 
 #lower 32 bits are used to specify the asset
 ASSET_BITCOIN = 1
@@ -20,5 +21,6 @@ NODE_ASSET_ID = ASSET_BITCOIN|ASSET_TESTNET_FLAG
 
 class getNodeInfo(InstachainRequestHandler):
     def processRequest(self):
+        deposit_address_derivation_path = "pkh(" + DEPOSIT_WALLET_MASTER_PUBKEY + "/44/1/(i/2147483647)/(i%2147483647))"
         self.result = ErrorMessage.build_error_message(ErrorMessage.ERROR_SUCCESS)
-        self.result['node_info'] = {'node_id': NODE_ID, 'node_name': 'Tesnet Node', 'asset_id': NODE_ASSET_ID}
+        self.result['node_info'] = {'node_id': NODE_ID, 'node_name': 'Tesnet Node', 'asset_id': NODE_ASSET_ID, 'deposit_address_derivation_path': deposit_address_derivation_path}
