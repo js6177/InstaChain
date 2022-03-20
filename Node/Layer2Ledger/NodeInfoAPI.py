87e57a0152e309198077cc499677aaebcd99ff73
James S <jamie.77.smith@protonmail.com>
2022-03-19 20:48:31 -0700

    add versioning to Layer2Ledger

--- Node/Layer2Ledger/NodeInfoAPI.py
+++ Node/Layer2Ledger/NodeInfoAPI.py
@@ -21,6 +21,7 @@ NODE_ASSET_ID = ASSET_BITCOIN|ASSET_TESTNET_FLAG
 
 class getNodeInfo(InstachainRequestHandler):
     def processRequest(self):
+        version = {'major_version': 1, 'minor_version': 0, 'patch_version': 0, 'API_version': 1}
         deposit_address_derivation_path = "pkh(" + DEPOSIT_WALLET_MASTER_PUBKEY + "/44/1/(i/2147483647)/(i%2147483647))"
         self.result = ErrorMessage.build_error_message(ErrorMessage.ERROR_SUCCESS)
-        self.result['node_info'] = {'node_id': NODE_ID, 'node_name': 'Tesnet Node', 'asset_id': NODE_ASSET_ID, 'deposit_address_derivation_path': deposit_address_derivation_path}
+        self.result['node_info'] = {'node_id': NODE_ID, 'node_name': 'Tesnet Node', 'asset_id': NODE_ASSET_ID, 'deposit_address_derivation_path': deposit_address_derivation_path, 'version': version}
