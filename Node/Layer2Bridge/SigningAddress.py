7cddbf0def96fe9e5865125661e1ce777e81a35e
James S <jamie.77.smith@protonmail.com>
2025-03-04 22:38:51 -0800

    Fix NODE_ID

--- Node/Layer2Bridge/SigningAddress.py
+++ Node/Layer2Bridge/SigningAddress.py
@@ -13,7 +13,7 @@ INSTRUCTION_LAYER1_AUDIT = 8 # instruction to get a withdrawal address
 
 DEFAULT_ELLIPTICAL_CURVE = ecdsa.SECP256k1
 DEFAULT_KEY_HASH_FUNCTION = hashlib.sha256
-NODE_ID = 'BbwLnyLR9eVjL2qb'
+NODE_ID = 'BbwLnyLR9eVjL2qc'
 
 def sign_string(signing_key, message):
         privkey = ecdsa.SigningKey.from_string(base58.b58decode(signing_key), curve=DEFAULT_ELLIPTICAL_CURVE, hashfunc=DEFAULT_KEY_HASH_FUNCTION)
