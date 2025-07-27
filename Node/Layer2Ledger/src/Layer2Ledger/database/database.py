38c17178a2ce8c73da61a730b023c293f6a66c5e
James S <jamie.77.smith@protonmail.com>
2025-07-26 17:06:45 -0700

    Use sqlalchemy.orm instead of sqlalchemy.ext.declarative to import declarative_base

--- Node/Layer2Ledger/src/Layer2Ledger/database/database.py
+++ Node/Layer2Ledger/src/Layer2Ledger/database/database.py
@@ -1,6 +1,6 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from typing import Generator
 from Layer2Ledger.config.config import config
