f46269c3d445fa3b6f0522f3d7eabe242d29bff6
James S <jamie.77.smith@protonmail.com>
2025-12-06 17:10:57 -0800

    Fix tailwind

--- frontend/wallet/apps/web/vite.config.ts
+++ frontend/wallet/apps/web/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
